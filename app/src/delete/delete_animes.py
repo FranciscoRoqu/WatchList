35b41363d1f953deee4806f7e2acfedb6d85fffc
Francisco Roque <francisco.roqu32007@gmail.com>
2025-02-26 12:10:10 +0000

    Atualização das operações CRUD

--- app/src/delete/delete_animes.py
+++ app/src/delete/delete_animes.py
@@ -1,21 +1,28 @@
 def delete_animes():
-    
     from InquirerPy import inquirer
     from InquirerPy.validator import NumberValidator
     from InquirerPy.validator import EmptyInputValidator
-    from src.connection import cursor,commit,close
+    from src.connection import cursor,commit
     cur = cursor()
 
     cur.execute("SELECT id, anime FROM animes")
     animes = cur.fetchall()
+
+    escolhas = []
+    for valor in animes:
+        anime_formatado = str(valor[0]) + "-> " + str(valor[1]) 
+        escolhas.append(anime_formatado)
     animes_para_apagar = inquirer.fuzzy(
         message="Selecione os animes que deseja apagar: ",
-        choices=animes,
+        choices=escolhas,
         multiselect=True,
         validate=lambda resultado: len(resultado) >= 1,
         invalid_message="Por favor selecione um valor",
         instruction="Use as setas para navegar, TAB para selecionar e ENTER para confirmar."
     ).execute()
-
+    i = 0
+    for valor in animes_para_apagar:
+        anime = valor[i]
+        anime_split = anime.split("-")
+        cur.execute('DELETE FROM animes WHERE id=' + anime_split[0])
     commit()
-    close()
--- app/src/delete/delete_visualizadores.py
+++ app/src/delete/delete_visualizadores.py
@@ -1,13 +1,28 @@
 def delete_visualizadores():
-    
     from InquirerPy import inquirer
     from InquirerPy.validator import NumberValidator
     from InquirerPy.validator import EmptyInputValidator
-    from src.connection import cursor,commit,close
+    from src.connection import cursor,commit
     cur = cursor()
 
-    nome = inquirer.text(message="Introduza o nome do visualizador: ", validate=EmptyInputValidator(message="Por fazor introduza um valor")).execute()
-    cur.execute("DELETE FROM visualizadores WHERE nome_visualizador = ?", (nome))
+    cur.execute("SELECT id, nome_visualizador FROM visualizadores")
+    visualizadores = cur.fetchall()
 
+    escolhas = []
+    for valor in visualizadores:
+        anime_formatado = str(valor[0]) + "-> " + str(valor[1]) 
+        escolhas.append(anime_formatado)
+    visualizadores_para_apagar = inquirer.fuzzy(
+        message="Selecione os visualizadores que deseja apagar: ",
+        choices=escolhas,
+        multiselect=True,
+        validate=lambda resultado: len(resultado) >= 1,
+        invalid_message="Por favor selecione um valor",
+        instruction="Use as setas para navegar, TAB para selecionar e ENTER para confirmar."
+    ).execute()
+    i = 0
+    for valor in visualizadores_para_apagar:
+        visualizador = valor[i]
+        visualizador_split = visualizador.split("-")
+        cur.execute('DELETE FROM visualizadores WHERE id=' + visualizador_split[0])
     commit()
-    close()
