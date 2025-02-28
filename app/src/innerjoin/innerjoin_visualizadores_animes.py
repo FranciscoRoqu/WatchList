74d3a6458efa80fa8802e13aad795237373fec76
Francisco Roque <francisco.roqu32007@gmail.com>
2025-02-28 09:20:13 +0000

    Correção do formato do texto

--- app/src/innerjoin/innerjoin_visualizadores_animes.py
+++ app/src/innerjoin/innerjoin_visualizadores_animes.py
@@ -1,13 +1,11 @@
 def innerjoin_visualizadores_animes():
-    from src.connection import cursor,close
+    from src.connection import cursor
 
     cur = cursor()
 
-    cur.execute('''SELECT * FROM animes INNER JOIN visualizadores ON animes.id = visualizadores.id''')
+    cur.execute('''SELECT * FROM visualizadores INNER JOIN animes ON visualizadores.id = animes.id''')
 
     resultados = cur.fetchall()
     
     for vis_anime in resultados:
         print(vis_anime)
-
-    close()
